147e11e6df36a08664bff64fb0b5c6ce56a64b0f
Mateusz Matusiak <mateusz.matusiak@likims.com>
2022-11-06 17:30:18 +0100

    Add facebook auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,59 @@
-import 'package:be_save/widget_tree.dart';
+import 'package:be_save/login/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  // const App({super.key});
+
+  // final _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: const WidgetTree(),
-    );
+    return FutureBuilder(
+        // future: _initialization,
+        builder: (context, snapshot) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        home: EmailLogin(),
+      );
+    });
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    testFirebase();
+    super.initState();
+  }
+
+  testFirebase() async {
+    final initApp = await Firebase.initializeApp();
+    print(initApp);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
